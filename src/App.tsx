7d51d2cc26b930b3e4b661ef6efcf2f030449f7c
Lukas656 <le.lu.ka2018@gmail.com>
2024-07-10 14:55:17 -0300

    feat: Construindo Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
+import { MapPin, Calendar, ArrowRight } from 'lucide-react'
+
 export function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello word</h1>
-      
-    </>
+    <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
+      <div className="max-w-3xl w-full px-6 text-center space-y-10">
+        <div className='flex flex-col items-center gap-3'>
+        <img src='/logo.svg' alt='plann.er'/>
+        <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua Viagem!</p>
+        </div>
+
+        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+          <div className='flex items-center gap-2 flex-1'>
+            <MapPin className='size-5 text-zinc-400' />
+            <input placeholder="Para onde Você vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none " />
+          </div>
+          <div className='flex items-center gap-2'>
+            <Calendar className='size-5 text-zinc-400' />
+            <input placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40  outline-none" />
+          </div>
+
+          <div className='w-px h-6 bg-zinc-800' />
+
+          <button className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
+            Continuar
+            <ArrowRight className='size-5' />
+          </button>
+        </div>
+
+        <p className="text-sm text-zinc-500">
+          Ao planejar sua viagem pela plann.er você automaticamente concorda <br /> com nossos
+          <a href="#" className="text-zinc-300 underline">termos de uso</a>
+          e
+          <a href="#" className="text-zinc-300 underline">politica de privacidade</a>
+        </p>
+      </div>
+    </div>
   )
 }
 
